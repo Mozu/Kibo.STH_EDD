79cf7194835f8a110f6e89e72f54452dd1e7f456
Travis Patrick <travisjpatrick@gmail.com>
2025-04-15 14:54:26 -0500

    COM-9308 automatically setup API Extension when app is installed to tenant (#4)
    Co-authored-by: Travis Patrick <travis.patrick@kibocommerce.com>

---
+++ assets/src/platform.applications.manifest.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  'appInstall': {
+    actionName: 'embedded.platform.applications.install',
+    customFunction: require('./domains/platform.applications/embedded.platform.applications.install')
+  }
+};
